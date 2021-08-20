1840e70bff0f299a45d14c34d0dadce6767ccc93
Gary J <gary-yrag@hotmail.com>
2021-08-19 23:08:31 -0500

    SARA

--- src/App.js
+++ src/App.js
@@ -6,16 +6,36 @@ import store from "./store";
 import Company from "./Component/Company"
 import CompanyList from "./Component/CompanyList"
 
+import imgg from './G.png';
+
 //import {BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom"
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
 
 function App() {
   return (
     <Provider store={store}>
+      <div>
+      <div >
+      <div className="ta">TE AMO MUCHO MUCHO MUCHO MUCHO. PERDONEME POR NO HABLAR, NOO ME DEJE EN EL MUNDO</div>
+        <div className="imgg2">
+          <img src={imgg} className="imgg2"></img>
+        </div>
+        <div className="ta">TE AMO</div>
+      </div>
       <div className="App">
         <Router>
 
           <div>
+          <ul>
+              <li>
+                <Link to="homes">Sara</Link>
+              </li>
+              <li>
+                <Link to="lists">Gary</Link>
+              </li>
+            </ul>
+
+  {/*          
             <ul>
               <li>
                 <Link to="home">Home</Link>
@@ -24,6 +44,9 @@ function App() {
                 <Link to="list">Companys</Link>
               </li>
             </ul>
+
+*/}
+
           </div>
 
           <Switch>
@@ -32,6 +55,9 @@ function App() {
           </Switch>          
         </Router>
       </div>
+      </div>
+
+     
 
     </Provider>
   );
