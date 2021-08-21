579bd3a3ad506eb4efa874d98abc7c308c88bb4f
Gary J <gary-yrag@hotmail.com>
2021-08-20 23:08:35 -0500

    SARA

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Company from "./Component/Company"
 import CompanyList from "./Component/CompanyList"
 
 import imgg from './G.png';
+import imgg2 from './imggs/img20082021.png';
 
 //import {BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom"
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
@@ -15,8 +16,24 @@ function App() {
   return (
     <Provider store={store}>
       <div>
+
       <div >
-      <div className="ta">TE AMO MUCHO MUCHO MUCHO MUCHO. PERDONEME POR NO HABLAR, NOO ME DEJE EN EL MUNDO</div>
+        <div className="ta2">          
+          BESITOOOOOOOOOOS, BESOOOOOOOOOOOOOOOTES BISCOCHO HERMOSA
+        </div>
+        <div className="imgg2">
+          <img src={imgg2} className="imgg2" alt="SADA"></img>
+        </div>
+        <div className="ta2">TE EXTAÑO COMO NUNCA</div>
+        <h1>20/08/2021</h1>
+      </div>
+
+
+      <div className="nomostrar">
+        <div className="ta">
+          <h1>19/08/2021</h1>
+          TE AMO MUCHO MUCHO MUCHO MUCHO. PERDONEME POR NO HABLAR, NOO ME DEJE EN EL MUNDO
+        </div>
         <div className="imgg2">
           <img src={imgg} className="imgg2" alt="AMOR"></img>
         </div>
