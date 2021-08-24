4617c83d5ec96c21e03968c894fd4b6c8432e7a0
Gary J <gary-yrag@hotmail.com>
2021-08-23 23:38:41 -0500

    SADA5

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import imgg from './G.png';
 import imgg2 from './imggs/img20082021.png';
 import imgg3 from './imggs/img21082021.png'; 
 import imgg4 from './imggs/img22082021.png';
+import imgg5 from './imggs/img23082021.png';
+
 
 //import {BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom"
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
@@ -20,7 +22,19 @@ function App() {
     <Provider store={store}>
       <div>
 
-      <div className="ta4">
+      <div className="ta5">
+        <div className="ta">          
+         TE AMO, ME AMAS ME AMAS ME AMAS ME AMAS ????????????????????????????, 
+        </div>
+        <div className="imgg2">
+          <img src={imgg5} className="imgg2" alt="SARA"></img>
+        </div>
+        <div className="ta">TE VEO LEJOOOOOOOOOOOOOOOOOOOOOOOOOOOOS, YA NO TE VEO, CUANDO ME MUERA ESTARAS AHY SIEMPRE</div>
+        <h1>23/08/2021</h1>
+      </div>
+
+
+      <div className="nomostrar">
         <div className="ta3">          
          NO ME HE OLVIDO DE TI, LAS COSITAS QUE HAGO TAL VEZ SON POCO, PERO SI ES DE TU AGRADO SOLO DISFRUTA LA IMAGEN Y SONRIELE POR QUE TODO LO QUE HE VIVIDO CON USD PARA MI LO HA SIDO TODO, Y CUANDO ESTE BIEN VIEJITA Y YO YA NO ESTE TE VAS A REIR, DE LAS TONTERIAS QUE HACIA POR TI 
         </div>
